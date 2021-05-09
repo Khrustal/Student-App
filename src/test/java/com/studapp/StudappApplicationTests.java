35e0d64083f787c41acf5d6270e54c22ae10459b
Artem Khrustal <artemkhrustal@gmail.com>
2021-05-09 17:23:50 +0300

    Added page tests

--- src/test/java/com/studapp/StudappApplicationTests.java
+++ src/test/java/com/studapp/StudappApplicationTests.java
@@ -1,13 +1,70 @@
 package com.studapp;
 
+import com.studapp.controller.StudentController;
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class StudappApplicationTests {
 
+    @Autowired
+    StudentController controller;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @ClassRule
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:11.1")
+            .withDatabaseName("student_app")
+            .withUsername("postgre")
+            .withPassword("Bosporez3");
+
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
     @Test
-    void contextLoads() {
+    public void shouldLoadHome() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+
+    @Test
+    public void shouldLoadCreate() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/create"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("create"));
+    }
+
+    @Test
+    public void shouldLoadStudent() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/student"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("student"));
     }
 
 }
